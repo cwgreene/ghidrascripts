4db1e94ca998c65b3e32d38cd4f96f0b71f0394a
Chris Greene <archgoon@gmail.com>
2020-06-04 00:59:15 -0700

    fix spaces.

--- FindLibcCalls.java
+++ FindLibcCalls.java
@@ -35,106 +35,108 @@ import ghidra.program.model.pcode.*;
 import ghidra.program.model.address.*;
 
 public class FindLibcCalls extends GhidraScript {
-   
-   private DecompInterface decomp;
-   
+    
+    private DecompInterface decomp;
+    
     class FunctionCall {
-      ClangFuncNameToken funcName;
-      List<ClangNode> arguments;
-      ClangStatement statement;
-      public FunctionCall(ClangFuncNameToken funcName, List<ClangNode> arguments, ClangStatement statement) {
-         this.funcName = funcName;
-         this.arguments = arguments;
-         this.statement = statement;
-      }
-   }
-   
-   private FunctionCall analyzeCall(ClangStatement clangStatement) {
-      ClangFuncNameToken funcName = null;
-      List<ClangNode> arguments = new ArrayList<>();
-      for (int i = 0; i < clangStatement.numChildren(); i++ ) {
-         ClangNode child = clangStatement.Child(i);
-         if (child instanceof ClangOpToken) {
-            ClangOpToken optoken = (ClangOpToken) child;
-            if (optoken.toString().contentEquals(",")) {
-               continue;
+        ClangFuncNameToken funcName;
+        List<ClangNode> arguments;
+        ClangStatement statement;
+        public FunctionCall(ClangFuncNameToken funcName, List<ClangNode> arguments, ClangStatement statement) {
+            this.funcName = funcName;
+            this.arguments = arguments;
+            this.statement = statement;
+        }
+    }
+    
+    private FunctionCall analyzeCall(ClangStatement clangStatement) {
+        ClangFuncNameToken funcName = null;
+        List<ClangNode> arguments = new ArrayList<>();
+        for (int i = 0; i < clangStatement.numChildren(); i++ ) {
+            ClangNode child = clangStatement.Child(i);
+            if (child instanceof ClangOpToken) {
+                ClangOpToken optoken = (ClangOpToken) child;
+                if (optoken.toString().contentEquals(",")) {
+                    continue;
+                }
+                arguments.add(optoken);
+            } else if (child instanceof ClangVariableToken) {
+                arguments.add(child);
+            } else if (child instanceof ClangFuncNameToken) {
+                funcName = (ClangFuncNameToken) child;
             }
-            arguments.add(optoken);
-         } else if (child instanceof ClangVariableToken) {
-            arguments.add(child);
-         } else if (child instanceof ClangFuncNameToken) {
-            funcName = (ClangFuncNameToken) child;
-         }
-      }
-      return new FunctionCall(funcName, arguments, clangStatement);
-   }
-   
-   private List<FunctionCall> findCallSites(Function func) {
-      List<FunctionCall> result = new ArrayList<>();
-       DecompileResults res = decomp.decompileFunction(func, 60, monitor);
-       List<ClangNode> tokens = new ArrayList<>();
-       res.getCCodeMarkup().flatten(tokens);
-       for(ClangNode token : tokens) {
-          if (token instanceof ClangFuncNameToken) {
-             if (token.Parent() instanceof ClangStatement) {
-                 ClangStatement statement = (ClangStatement) token.Parent();
-                 if (statement.getPcodeOp().getMnemonic().equals("CALL")) {
-                    result.add(analyzeCall(statement));
-                 }
-             }
-          }
-       }
-       return result;
-   }
+        }
+        return new FunctionCall(funcName, arguments, clangStatement);
+    }
+    
+    private List<FunctionCall> findCallSites(Function func) {
+        List<FunctionCall> result = new ArrayList<>();
+        DecompileResults res = decomp.decompileFunction(func, 60, monitor);
+        List<ClangNode> tokens = new ArrayList<>();
+        res.getCCodeMarkup().flatten(tokens);
+        for(ClangNode token : tokens) {
+            if (token instanceof ClangFuncNameToken) {
+                if (token.Parent() instanceof ClangStatement) {
+                    ClangStatement statement = (ClangStatement) token.Parent();
+                    if (statement.getPcodeOp().getMnemonic().equals("CALL")) {
+                        result.add(analyzeCall(statement));
+                    }
+                }
+            }
+        }
+        return result;
+    }
 
     public void run() throws Exception {
-       println("Hello world!");
-       println(currentProgram == null ? "null" : currentProgram.toString());
-       decomp = setUpDecompiler(currentProgram);
-       FunctionIterator funcs = currentProgram.getListing().getFunctions(true);
-       for (Function func : funcs) {
-          if (!func.isThunk()) {
-             println("-"+func.getName());
-             /*for (Function calledFunc : func.getCalledFunctions(monitor)) {
-                String isExternal = calledFunc.isThunk() ? "*" : "";
-                println("--"+ isExternal+ " " + calledFunc.getName());
-                
-             }*/
-             for (FunctionCall call : findCallSites(func)) {
-                println("-  " + call.statement.toString());
-             }
-          }
-       }  
+        println("Hello world!");
+        println(currentProgram == null ? "null" : currentProgram.toString());
+        decomp = setUpDecompiler(currentProgram);
+        FunctionIterator funcs = currentProgram.getListing().getFunctions(true);
+        for (Function func : funcs) {
+            if (!func.isThunk()) {
+                println("-"+func.getName());
+                /*for (Function calledFunc : func.getCalledFunctions(monitor)) {
+                    String isExternal = calledFunc.isThunk() ? "*" : "";
+                    println("--"+ isExternal+ " " + calledFunc.getName());
+                    
+                }*/
+                for (FunctionCall call : findCallSites(func)) {
+                    println("-  " + call.statement.toString());
+                }
+            }
+        }
+        
     }
-   private DecompInterface setUpDecompiler(Program program) {
-      DecompInterface decompInterface = new DecompInterface();
+    
+    private DecompInterface setUpDecompiler(Program program) {
+        DecompInterface decompInterface = new DecompInterface();
 
-      // call it to get results
-      if (!decompInterface.openProgram(currentProgram)) {
-         println("Decompile Error: " + decompInterface.getLastMessage());
-         return null;
-      }
+        // call it to get results
+        if (!decompInterface.openProgram(currentProgram)) {
+            println("Decompile Error: " + decompInterface.getLastMessage());
+            return null;
+        }
 
-      DecompileOptions options;
-      options = new DecompileOptions();
-      
-      println(state.getTool() == null ? "No tool!" : state.getTool().getName());
-      PluginTool currentTool = state.getTool();
-      if (currentTool != null) {
-         OptionsService service = state.getTool().getService(OptionsService.class);
-         if (service != null) {
-            ToolOptions opt = service.getOptions("Decompiler");
-            options.grabFromToolAndProgram(null, opt, program);
-         }
-      }
-      decompInterface.setOptions(options);
+        DecompileOptions options;
+        options = new DecompileOptions();
+        
+        println(state.getTool() == null ? "No tool!" : state.getTool().getName());
+        PluginTool currentTool = state.getTool();
+        if (currentTool != null) {
+            OptionsService service = state.getTool().getService(OptionsService.class);
+            if (service != null) {
+                ToolOptions opt = service.getOptions("Decompiler");
+                options.grabFromToolAndProgram(null, opt, program);
+            }
+        }
+        decompInterface.setOptions(options);
 
-      decompInterface.toggleCCode(true);
-      decompInterface.toggleSyntaxTree(true);
-      decompInterface.setSimplificationStyle("decompile");
+        decompInterface.toggleCCode(true);
+        decompInterface.toggleSyntaxTree(true);
+        decompInterface.setSimplificationStyle("decompile");
 
-      return decompInterface;
-   }
+        return decompInterface;
+    }
 
     
 
