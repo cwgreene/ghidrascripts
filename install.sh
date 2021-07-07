5f8ad8e65d7ddd02f2cc08252b530fbff5331a12
Chris Greene <archgoon@gmail.com>
2021-07-07 13:08:33 -0700

    Fix up install script to work with 9.2
    Fixes https://github.com/cwgreene/ghidrascripts/issues/2

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
+echo "Need at least ghidra 9.2 to have this work correctly"
 echo -n "Ghidra Directory (used to get version): "
 read GHIDRA_INSTALL_DIRECTORY
 GHIDRA_VERSION=$(basename $GHIDRA_INSTALL_DIRECTORY)
-GHIDRA_PLUGINS_DIR=$HOME/.ghidra/.$GHIDRA_VERSION/plugins
-echo "Ensuring $GHIDRA_PLUGINS_DIR exists"
-mkdir -p $GHIDRA_PLUGINS_DIR
+GHIDRA_PLUGINS_DIR="${GHIDRA_INSTALL_DIRECTORY}"/Ghidra/Features/Base/lib
 echo "Downloading jars into $GHIDRA_PLUGINS_DIR"
 for jar in jackson-core-2.11.3.jar jackson-databind-2.11.3.jar jackson-annotations-2.11.3.jar; do
     component=$(echo -n $jar | sed 's/[^-]*-\([^-]*\)-.*/\1/')
