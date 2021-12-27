328eb5fc0612594cd8a371054dd13b8c17b2db3d
Chris Greene <archgoon@gmail.com>
2021-12-27 03:39:49 +0000

    Add argument, fix chmod.

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 echo "Need at least ghidra 9.2 to have this work correctly"
-echo -n "Ghidra Directory (used to get version): "
-read GHIDRA_INSTALL_DIRECTORY
+if [ x"$1" -eq "x" ]; then
+	echo -n "Ghidra Directory (used to get version): "
+	read GHIDRA_INSTALL_DIRECTORY
+else
+	GHIDRA_INSTALL_DIRECTORY="$1"
+fi
 GHIDRA_VERSION=$(basename $GHIDRA_INSTALL_DIRECTORY)
 GHIDRA_PLUGINS_DIR="${GHIDRA_INSTALL_DIRECTORY}"/Ghidra/Features/Base/lib
 echo "Downloading jars into $GHIDRA_PLUGINS_DIR"
 for jar in jackson-core-2.11.3.jar jackson-databind-2.11.3.jar jackson-annotations-2.11.3.jar; do
     component=$(echo -n $jar | sed 's/[^-]*-\([^-]*\)-.*/\1/')
-    echo "Downloading $jar for component $component"
-    curl -s "https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-$component/2.11.3/$jar" -o "$GHIDRA_PLUGINS_DIR/$jar"
+    echo "Downloading $jar for component $component into $GHIDRA_PLUGINS_DIR/$jar"
+    echo curl -s "https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-$component/2.11.3/$jar" -o "$GHIDRA_PLUGINS_DIR/$jar"
+    curl  "https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-$component/2.11.3/$jar" -o "$GHIDRA_PLUGINS_DIR/$jar"
 done
